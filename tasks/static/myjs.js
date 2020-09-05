408e28b6f557fc2018dd9b0363438c0422902ca4
Your Name <georgebak2182@gmail.com>
2020-09-05 17:57:46 +0300

    -Adding project

---
+++ tasks/static/myjs.js
@@ -0,0 +1,9 @@
+function onclickDelete() {
+    Swal.fire({
+        icon: 'error',
+        title: 'You deleted a task',
+      })
+
+}
+console.log('Working..')
+console.log('Script Is Working Corectly')
