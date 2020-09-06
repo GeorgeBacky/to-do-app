1f58b840d96e6f211695977dcb56a48808c58366
Your Name <georgebak2182@gmail.com>
2020-09-06 03:15:08 +0300

    -Changed hole template and write from  0

--- tasks/static/myjs.js
+++ tasks/static/myjs.js
@@ -1,18 +1,21 @@
 function onclickDelete() {
-  Swal.fire(
-    'Deleted!',
-    'Your task is deleted!',
-    'success'
-  )
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Your task is deleted!',
+    showConfirmButton: false,
+    timer: 1500
+  })
 
 }
 // Success Update
 function onclickUpdate() {
-  Swal.fire(
-    'Success!',
-    'Your task is updated',
-    'success'
-  )
-}
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Your task is updated!',
+    showConfirmButton: false,
+    timer: 1500
+  })
 console.log('Working..')
 console.log('Script Is Working Corectly')
